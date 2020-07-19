a055b36bf6ba381d4b0a91bb85f0fe0b9f30c653
Markus Gilli <m.gi@gmx.ch>
2020-07-19 19:31:42 +0200

    Fix PipFile

--- frety/board.py
+++ frety/board.py
@@ -1,7 +1,7 @@
 class Board:
     NOTES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
-    def __init__(self, frets=12, length=628, strings=None):
+    def __init__(self, frets=13, length=628, strings=None):
         self._fret_count = frets
         self._mansur = length
         self._strings = strings or [('e', 1), ('b', 2), ('G', 3), ('D', 4), ('A,', 5), ('E,,', 6)]
